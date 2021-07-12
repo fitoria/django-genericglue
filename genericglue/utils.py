33cfa293b853c59eba9580a075e47e6cf48c94fa
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2021-07-12 09:55:44 -0600

    fix issue on genericglue and django 3.2

--- genericglue/utils.py
+++ genericglue/utils.py
@@ -6,6 +6,4 @@ def table_exists(table_name, database=DEFAULT_DB_ALIAS):
     Determines if the given table_name exists in the specified database.
     """
     connection = connections[database]
-    tables = connection.introspection.table_names()
-    converter = connection.introspection.table_name_converter
-    return converter(table_name) in tables
+    return table_name in connection.introspection.table_names()
